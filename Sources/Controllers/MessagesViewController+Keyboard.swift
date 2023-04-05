9ff90b3efcc991f5ac1c55f32bddb674be74ec0e
thomas <tomargue@gmail.com>
2023-04-05 23:11:07 +1000

    Fixing scrolling

--- Sources/Controllers/MessagesViewController+Keyboard.swift
+++ Sources/Controllers/MessagesViewController+Keyboard.swift
@@ -26,109 +26,113 @@ import InputBarAccessoryView
 import UIKit
 
 extension MessagesViewController {
-  // MARK: Internal
-
-  // MARK: - Register Observers
-
-  internal func addKeyboardObservers() {
-    keyboardManager.bind(inputAccessoryView: inputContainerView)
-    keyboardManager.bind(to: messagesCollectionView)
-
-    /// Observe didBeginEditing to scroll content to last item if necessary
-    NotificationCenter.default
-      .publisher(for: UITextView.textDidBeginEditingNotification)
-      .subscribe(on: DispatchQueue.global())
-      /// Wait for inputBar frame change animation to end
-      .delay(for: .milliseconds(200), scheduler: DispatchQueue.main)
-      .receive(on: DispatchQueue.main)
-      .sink { [weak self] notification in
-        self?.handleTextViewDidBeginEditing(notification)
-      }
-      .store(in: &disposeBag)
-
-    NotificationCenter.default
-      .publisher(for: UITextView.textDidChangeNotification)
-      .subscribe(on: DispatchQueue.global())
-      .receive(on: DispatchQueue.main)
-      .compactMap { $0.object as? InputTextView }
-      .filter { [weak self] textView in
-        textView == self?.messageInputBar.inputTextView
-      }
-      .map(\.text)
-      .removeDuplicates()
-      .delay(for: .milliseconds(50), scheduler: DispatchQueue.main) /// Wait for next runloop to lay out inputView properly
-      .sink { [weak self] _ in
-        self?.updateMessageCollectionViewBottomInset()
-
-        if !(self?.maintainPositionOnInputBarHeightChanged ?? false) {
-          self?.messagesCollectionView.scrollToLastItem()
+    // MARK: Internal
+    
+    // MARK: - Register Observers
+    
+    internal func addKeyboardObservers() {
+        keyboardManager.bind(inputAccessoryView: inputContainerView)
+        keyboardManager.bind(to: messagesCollectionView)
+        
+        /// Observe didBeginEditing to scroll content to last item if necessary
+        NotificationCenter.default
+            .publisher(for: UITextView.textDidBeginEditingNotification)
+            .subscribe(on: DispatchQueue.global())
+        /// Wait for inputBar frame change animation to end
+            .delay(for: .milliseconds(200), scheduler: DispatchQueue.main)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] notification in
+                // self?.handleTextViewDidBeginEditing(notification)
+            }
+            .store(in: &disposeBag)
+        
+        NotificationCenter.default
+            .publisher(for: UITextView.textDidChangeNotification)
+            .subscribe(on: DispatchQueue.global())
+            .receive(on: DispatchQueue.main)
+            .compactMap { $0.object as? InputTextView }
+            .filter { [weak self] textView in
+                textView == self?.messageInputBar.inputTextView
+            }
+            .map(\.text)
+            .removeDuplicates()
+            .delay(for: .milliseconds(50), scheduler: DispatchQueue.main) /// Wait for next runloop to lay out inputView properly
+            .sink { [weak self] _ in
+                self?.updateMessageCollectionViewBottomInset()
+                
+                if !(self?.maintainPositionOnInputBarHeightChanged ?? false) {
+                    // self?.messagesCollectionView.scrollToLastItem()
+                }
+            }
+            .store(in: &disposeBag)
+        
+        /// Observe frame change of the input bar container to update collectioView bottom inset
+        inputContainerView.publisher(for: \.center)
+            .receive(on: DispatchQueue.main)
+            .removeDuplicates()
+            .sink(receiveValue: { [weak self] _ in
+                self?.updateMessageCollectionViewBottomInset()
+            })
+            .store(in: &disposeBag)
+    }
+    
+    // MARK: - Updating insets
+    
+    /// Updates bottom messagesCollectionView inset based on the position of inputContainerView
+    internal func updateMessageCollectionViewBottomInset() {
+        let collectionViewHeight = messagesCollectionView.frame.maxY
+        let newBottomInset = collectionViewHeight - (inputContainerView.frame.minY - additionalBottomInset) -
+        automaticallyAddedBottomInset
+        let normalizedNewBottomInset = max(0, newBottomInset)
+        let differenceOfBottomInset = newBottomInset - messageCollectionViewBottomInset
+        
+        UIView.performWithoutAnimation {
+            guard differenceOfBottomInset != 0 else { return }
+            messagesCollectionView.contentInset.bottom = normalizedNewBottomInset
+            messagesCollectionView.verticalScrollIndicatorInsets.bottom = newBottomInset
         }
-      }
-      .store(in: &disposeBag)
 
-    /// Observe frame change of the input bar container to update collectioView bottom inset
-    inputContainerView.publisher(for: \.center)
-      .receive(on: DispatchQueue.main)
-      .removeDuplicates()
-      .sink(receiveValue: { [weak self] _ in
-        self?.updateMessageCollectionViewBottomInset()
-      })
-      .store(in: &disposeBag)
-  }
-
-  // MARK: - Updating insets
-
-  /// Updates bottom messagesCollectionView inset based on the position of inputContainerView
-  internal func updateMessageCollectionViewBottomInset() {
-    let collectionViewHeight = messagesCollectionView.frame.maxY
-    let newBottomInset = collectionViewHeight - (inputContainerView.frame.minY - additionalBottomInset) -
-      automaticallyAddedBottomInset
-    let normalizedNewBottomInset = max(0, newBottomInset)
-    let differenceOfBottomInset = newBottomInset - messageCollectionViewBottomInset
-
-    UIView.performWithoutAnimation {
-      guard differenceOfBottomInset != 0 else { return }
-      messagesCollectionView.contentInset.bottom = normalizedNewBottomInset
-      messagesCollectionView.verticalScrollIndicatorInsets.bottom = newBottomInset
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut) {
+            self.messagesCollectionView.contentOffset = .init(x: 0, y: self.messagesCollectionView.contentOffset.y + differenceOfBottomInset)
+        }
     }
-  }
-
-  // MARK: Private
-
-  /// UIScrollView can automatically add safe area insets to its contentInset,
-  /// which needs to be accounted for when setting the contentInset based on screen coordinates.
-  ///
-  /// - Returns: The distance automatically added to contentInset.bottom, if any.
-  private var automaticallyAddedBottomInset: CGFloat {
-    messagesCollectionView.adjustedContentInset.bottom - messageCollectionViewBottomInset
-  }
-
-  private var messageCollectionViewBottomInset: CGFloat {
-    messagesCollectionView.contentInset.bottom
-  }
-
-  /// UIScrollView can automatically add safe area insets to its contentInset,
-  /// which needs to be accounted for when setting the contentInset based on screen coordinates.
-  ///
-  /// - Returns: The distance automatically added to contentInset.top, if any.
-  private var automaticallyAddedTopInset: CGFloat {
-    messagesCollectionView.adjustedContentInset.top - messageCollectionViewTopInset
-  }
-
-  private var messageCollectionViewTopInset: CGFloat {
-    messagesCollectionView.contentInset.top
-  }
-
-  // MARK: - Private methods
-
-  private func handleTextViewDidBeginEditing(_ notification: Notification) {
-    guard
-      scrollsToLastItemOnKeyboardBeginsEditing,
-      let inputTextView = notification.object as? InputTextView,
-      inputTextView === messageInputBar.inputTextView
-    else {
-      return
+    
+    // MARK: Private
+    
+    /// UIScrollView can automatically add safe area insets to its contentInset,
+    /// which needs to be accounted for when setting the contentInset based on screen coordinates.
+    ///
+    /// - Returns: The distance automatically added to contentInset.bottom, if any.
+    private var automaticallyAddedBottomInset: CGFloat {
+        messagesCollectionView.adjustedContentInset.bottom - messageCollectionViewBottomInset
     }
-    messagesCollectionView.scrollToLastItem()
-  }
+    
+    private var messageCollectionViewBottomInset: CGFloat {
+        messagesCollectionView.contentInset.bottom
+    }
+    
+    /// UIScrollView can automatically add safe area insets to its contentInset,
+    /// which needs to be accounted for when setting the contentInset based on screen coordinates.
+    ///
+    /// - Returns: The distance automatically added to contentInset.top, if any.
+    private var automaticallyAddedTopInset: CGFloat {
+        messagesCollectionView.adjustedContentInset.top - messageCollectionViewTopInset
+    }
+    
+    private var messageCollectionViewTopInset: CGFloat {
+        messagesCollectionView.contentInset.top
+    }
+    
+    // MARK: - Private methods
+    
+    private func handleTextViewDidBeginEditing(_ notification: Notification) {
+        guard
+            scrollsToLastItemOnKeyboardBeginsEditing,
+            let inputTextView = notification.object as? InputTextView,
+            inputTextView === messageInputBar.inputTextView
+        else {
+            return
+        }
+        // messagesCollectionView.scrollToLastItem()
+    }
 }
